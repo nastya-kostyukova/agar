bf4c3ad0deb4a57fa23baf7c6f9b2f69299c4007
nastya-kostyukova <6608073happy>
2016-04-28 23:25:58 +0300

    changed user movement

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ io.sails.url = '/';
 
 var mainCanvas = $("#mainCanvas");
 var mealCanvas = $("#mealCanvas");
+var meals;
 
 $( document ).ready(function() {
   io.socket.put('/api/ws', { occupation: 'loading', x: 0, y: 0, radius: 50 }, function (resData, jwr) {
@@ -47,18 +48,47 @@ var circle = mainCanvas.drawArc({
   fillStyle: "green",
   x: 100, y: 100,
   radius: 50,
-  name: 'myDot',
-  mouseover: function(layer) {
-    $(this).animateLayer(layer, {
-      fillStyle: '#c33'
-    }, 500);
-  },
-  mouseout: function(layer) {
-    $(this).animateLayer(layer, {
-      fillStyle: '#fff'
-    }, 500);
-  }
+  name: 'myDot'
 });
+
+var timer = -1;
+
+function detectCircle() {
+  var myDot = mainCanvas.getLayer('myDot');
+  console.log(myDot);
+  return { x: myDot.x, y: myDot.y}
+}
+
+function moveToXY(x, y) {
+  if (timer > 0) {
+    clearInterval(timer);
+    timer = -1;
+  }
+  var fps = 1000 // 50 кадров в секунду
+  var canvas = document.getElementById('mainCanvas');
+  var myDot = mainCanvas.getLayer('myDot');
+  var dist = Math.sqrt((Math.pow(x- myDot.x, 2)) + Math.pow(y- myDot.y, 2));
+  var stepX = (x- myDot.x) / dist;
+  var stepY = (y- myDot.y) / dist;
+
+  timer = setInterval(function() {
+    var myDot = mainCanvas.getLayer('myDot');
+    var newDist = Math.sqrt((Math.pow(x - myDot.x, 2)) + Math.pow(y - myDot.y, 2));
+    if (newDist < 2) {
+      clearInterval(timer);
+      dist = 0;
+      stepX = 0;
+      stepY = 0;
+    } else {
+      clearPoints(meals);
+      mainCanvas.setLayer('myDot', {
+        x: '+='+stepX,
+        y: '+='+stepY
+      })
+      .drawLayers();
+    };
+  }, 1000 / fps)
+}
 
 mainCanvas.click(function(event) {
   var x = event.offsetX;
@@ -68,20 +98,18 @@ mainCanvas.click(function(event) {
     console.log(resData.test); // => 200
   });
 
-  mainCanvas.stopLayer('myDot');
-  mainCanvas.animateLayer('myDot', {
-      fillStyle: '#36b',
-      rotate: 30,
-      x: x,
-      y: y
-    }, 1000)
-    .drawLayers()
+  /* TO DO: По клику получаем список точек, кладем в переменную meals. В функции moveToXY проверяем попадаем ли на какую-нибудь точку и удаляем эту точку на клиенте из массива meals */
+  /* При удалении вызвать io.socket.put с текущим массивом точек meals, он должен вызвать на бэкенде blast, который разошлет новый список точек */
 
   io.socket.on('move_of_user', function onServerSentEvent (msg) {
     console.log(msg.msg);
-    drawDots(msg.points);
-    clearPoints(msg.removingPints)
+    meals = msg.removingPints;
+    //drawDots(msg.points);
+    //clearPoints(msg.removingPints)
   });
+
+
+  moveToXY(x, y);
 });
 
 var dot;
